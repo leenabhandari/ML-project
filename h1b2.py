30a4f3d693cce82cb118a670c98c5f984948d804
Leena <leenabhandari1@gmail.com>
2018-02-13 17:53:52 +0530

    Add files via upload

--- h1b2.py
+++ h1b2.py
@@ -3,14 +3,14 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
-from sklearn import tree
+#from sklearn import tree
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.svm import SVC
-from sklearn.preprocessing import Imputer
-from sklearn.feature_extraction import FeatureHasher
-from sklearn.feature_extraction.text import HashingVectorizer
+#from sklearn.svm import SVC
+#from sklearn.preprocessing import Imputer
+#from sklearn.feature_extraction import FeatureHasher
+#from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
@@ -21,8 +21,9 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
-from pickle import dump
-from pickle import load
+#from pickle import dump
+#from pickle import load
+#from sklearn.model_selection import GridSearchCV
 import string
 
 
@@ -31,7 +32,7 @@ data=read_csv('h1b_kaggle.csv')
 #remove data with missiing values
 index_miss=data.lat.isnull()
 data=data[index_miss!= True]
-index_miss=data.lat.isnull()
+index_miss=data.lon.isnull()
 data=data[index_miss!= True]
 index_miss=data.PREVAILING_WAGE.isnull()
 data=data[index_miss!= True]
@@ -47,7 +48,7 @@ data1=data
 data1=data1.reindex(np.random.permutation(data1.index)) #shuffle all data
 
 ds=data1.values
-Y_data=ds[:,1]
+Y_data=ds[:,1] #CASE_STATUS
 
 full_time=ds[:,5]
 #convert full_time value to 0 & 1
@@ -65,7 +66,7 @@ data_lon=np.array([]) #lon
 data_lat=np.array([]) #lat
 data_full=np.array([]) #FULL_TIME
 data_ws=np.array([]) #WORKSITE
-data_soc=np.array([])
+#data_soc=np.array([])
 
 
 #append rows ( 10000 of each category)
@@ -80,7 +81,7 @@ for i in range(Y_data.size):
   data_lat=np.append(data_lat,ds[i,10])
   data_full=np.append(data_full,full_time[i])
   data_ws=np.append(data_ws,ds[i,8])
-  data_soc=np.append(data_soc,ds[i,3])
+  #data_soc=np.append(data_soc,ds[i,3])
   count_cert=count_cert+1
 
 count_with=0
@@ -94,7 +95,7 @@ for i in range(Y_data.size):
   data_lat=np.append(data_lat,ds[i,10])
   data_full=np.append(data_full,full_time[i])
   data_ws=np.append(data_ws,ds[i,8])
-  data_soc=np.append(data_soc,ds[i,3])
+  #data_soc=np.append(data_soc,ds[i,3])
   count_with=count_with+1		
 
 
@@ -109,7 +110,7 @@ for i in range(Y_data.size):
   data_lat=np.append(data_lat,ds[i,10])
   data_full=np.append(data_full,full_time[i])
   data_ws=np.append(data_ws,ds[i,8])
-  data_soc=np.append(data_soc,ds[i,3])
+  #data_soc=np.append(data_soc,ds[i,3])
   count_certwith=count_certwith+1
 
   
@@ -124,7 +125,7 @@ for i in range(Y_data.size):
   data_lat=np.append(data_lat,ds[i,10])
   data_full=np.append(data_full,full_time[i])
   data_ws=np.append(data_ws,ds[i,8])
-  data_soc=np.append(data_soc,ds[i,3])
+  #data_soc=np.append(data_soc,ds[i,3])
   count_den=count_den+1
   
 
@@ -161,9 +162,29 @@ np.random.shuffle(data2)
 #separate out X&Y
 Y_new=data2[:,0]
 X_new=data2[:,1:63]  
-kfold = KFold(n_splits=10, random_state=7)  
+#kfold = KFold(n_splits=10, random_state=7)  
 X_train, X_test, Y_train, Y_test = train_test_split(X_new, Y_new, test_size=0.10,random_state=7)
 # X_wage=ds[:,6] #prevailing wage
 
+model=RandomeForestClassifier()
+prams={'max_depth':[None,1,2,3,4],'n_estimators':[10,20,30,50,1,100]}
+grid=GridSearchCV(model,prams)
+grid.fit(X_new,Y_new)
+print grid.best_score_ #0.33001699830017
+print grid.best_estimator_.n_estimators #100
 
+model=DecisionTreeClassifier()
+prams={'min_samples_leaf':(1,2,3,5),'max_depth':[None,1,2,3]}
+grid.best_score_ #0.3263173682631737
+grid.best_estimator_.min_samples_leaf #1
 
+model=AdaBoostClassifier()
+prams={'n_estimators':[50,10,100],'learning_rate':[1,2]}
+grid.best_score_  #0.31916808319168083
+grid.best_estimator_.learning_rate #1
+grid.best_estimator_.n_estimators #100
+
+model=KNeighborsClassifier()
+prams={'n_neighbors':[4,5,8]}
+grid.best_score_ #0.3191930806919308
+grid.best_estimator_.n_neighbors #4
